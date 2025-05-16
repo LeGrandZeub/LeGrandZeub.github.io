e23fe0eeed5fe7099cbeec4b1fd856390e72ea98
Louqo <108452671+LeGrandZeub@users.noreply.github.com>
2025-05-16 16:30:44 +0200

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -32,7 +32,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "c4d4189579f7e059765b099b41517702
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "firebase-messaging-sw.js": "36bbdb55b86d5fdbce74cdb7356e8eb6",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "d9c6e017a0f3be84e750fb9148ed8302",
+"flutter_bootstrap.js": "dedb041183df4e6e79b5b2988473f3de",
 "google_pay.js": "75145ff8961307325abff21ac71fa2cf",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
@@ -40,7 +40,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "c4d4189579f7e059765b099b41517702
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "c9968bf99d4116d59341b5ad24c34930",
 "/": "c9968bf99d4116d59341b5ad24c34930",
-"main.dart.js": "8dcf3eec857a2963f4bdef7a17007382",
+"main.dart.js": "aa1c54ca759ce472660ae1c73a17a5de",
 "manifest.json": "95f9c632329be8cc96133b633cfc390a",
 "version.json": "00a6b2f5928cc030e03849eca5c4f699"};
 // The application shell files that are downloaded before a service worker can
