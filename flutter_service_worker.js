5efb6adff919aae35cd4d333e356d647e8598e0e
Louqo <108452671+LeGrandZeub@users.noreply.github.com>
2025-06-11 15:52:06 +0200

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -32,7 +32,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "c4d4189579f7e059765b099b41517702
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "firebase-messaging-sw.js": "36bbdb55b86d5fdbce74cdb7356e8eb6",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "85497f5e7c12dc328c88ddf1e64c6484",
+"flutter_bootstrap.js": "2ad872cd6c2714797e087a4d5d836dd3",
 "google_pay.js": "75145ff8961307325abff21ac71fa2cf",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
